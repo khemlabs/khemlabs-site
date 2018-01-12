3788bc6c4075c1a8fd2d9e3cbdfbc3867272f770
DNAngeluS <luciomf@gmail.com>
2018-01-12 14:46:52 -0300

    Changed strings scroller

--- public/js/app.js
+++ public/js/app.js
@@ -1,64 +1,63 @@
-(function($, Materialize){
-  
-  var scroller = new letterScroller('console-stdout', [
+(function ($, Materialize) {
+
+	var scroller = new letterScroller('console-stdout', [
 		"Web Services",
 		"Mobile Apps",
-    "Single Page Applications",
-		"Node.js v4",
-		"Web Mobile",
-		"ES2015",
+		"ReactNative",
 		"Ionic",
-    "AngularJS",
-    "React",
-    "MongoDB",
-    "Linux",
-    "Nginx",
-    "Express",
-    "PostgreSQL"
-  ]);
-         
-  $(".button-collapse").sideNav();
-  
-  $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
+		"Single Page Applications",
+		"Node.js",
+		"ReactJS",
+		"Responsive & Mobile First Web",
+		"Docker",
+		"MongoDB",
+		"PostgreSQL",
+		"Linux",
+		"OpenSource"
+	]);
 
-      var target = this.hash;
-      var $target = $(target);
+	$(".button-collapse").sideNav();
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top - 50
-      }, 600, 'swing', function () {
-          window.location.hash = target;
-      });
-  });
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
 
-  $("#message-form").on("submit", function(){
-    if($('#message-form')[0].checkValidity()){
-      $.ajax({
-        type: "post",
-        contentType: "application/x-www-form-urlencoded",
-        url: "/message",
-        data: $('#message-form').serialize()
-      }).then(function(data){
+		var target = this.hash;
+		var $target = $(target);
 
-        $("#send-btn")
-          .addClass("disabled")
-          .attr("disabled", true);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top - 50
+		}, 600, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
 
-        setTimeout(function(){
-          $("#send-btn")
-            .removeClass("disabled")
-            .removeAttr("disabled");
-        }, 60000);
+	$("#message-form").on("submit", function () {
+		if ($('#message-form')[0].checkValidity()) {
+			$.ajax({
+				type: "post",
+				contentType: "application/x-www-form-urlencoded",
+				url: "/message",
+				data: $('#message-form').serialize()
+			}).then(function (data) {
 
-        Materialize.toast("Message sent", 3000, "green");
+				$("#send-btn")
+					.addClass("disabled")
+					.attr("disabled", true);
 
-        $('#message-form')[0].reset();
-      });
-    }else{
-        Materialize.toast("Please fill the form data properly.", 3000, "red");
-    }
-    return false;
-  });
+				setTimeout(function () {
+					$("#send-btn")
+						.removeClass("disabled")
+						.removeAttr("disabled");
+				}, 60000);
+
+				Materialize.toast("Message sent", 3000, "green");
+
+				$('#message-form')[0].reset();
+			});
+		} else {
+			Materialize.toast("Please fill the form data properly.", 3000, "red");
+		}
+		return false;
+	});
 
 })(jQuery, Materialize);
