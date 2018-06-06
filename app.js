abd0f9382e761fa3d919fa54c40393cdf142c43f
Pablo Gambetta <gambeita@gmail.com>
2018-06-06 17:45:59 -0300

    fix fonts and styles error

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const express = require('express'),
 	ejs = require('ejs'),
 	messageRouter = require('./routes/message'),
 	homeRouter = require('./routes/home'),
-	log = require('./services/Log'),
-	config = require('./config/config');
+	log = require('./services/Log');
 
 // Configure app
 const server = http.createServer(app);
@@ -48,18 +47,6 @@ app.use('/', homeRouter);
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
-
-config.CLIENT_LIBS.forEach(lib => {
-	const staticPath = `${__dirname}/${lib.path}`;
-	log.info(`path (${lib.path}) added as static path`, 'app.js', 'static_libs');
-	app.use(`/${lib.path}`, express.static(staticPath));
-});
-
-config.CLIENT_STYLES_LIBS.forEach(style => {
-	const staticPath = `${__dirname}/${style.path}`;
-	log.info(`path (${style.path}) added as static path`, 'app.js', 'static_styles_libs');
-	app.use(`/${style.path}`, express.static(staticPath));
-});
 
 // listen
 server.listen(process.env.PORT || 3000, function() {
