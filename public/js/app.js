81cd2bf61570528cd649f7385f91974ab314dd95
Pablo Gambetta <gambeita@gmail.com>
2018-06-06 21:01:17 -0300

    carrusel added missing fonts added

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,71 @@
-(function ($, Materialize) {
-
+(function($, Materialize) {
 	var scroller = new letterScroller('console-stdout', [
-		"Web Services",
-		"Mobile Apps",
-		"ReactNative",
-		"Ionic",
-		"Single Page Applications",
-		"Node.js",
-		"ReactJS",
-		"Responsive & Mobile First Web",
-		"Docker",
-		"MongoDB",
-		"PostgreSQL",
-		"Linux",
-		"OpenSource"
+		'Web Services',
+		'Mobile Apps',
+		'ReactNative',
+		'Ionic',
+		'Single Page Applications',
+		'Node.js',
+		'ReactJS',
+		'Responsive & Mobile First Web',
+		'Docker',
+		'MongoDB',
+		'PostgreSQL',
+		'Linux',
+		'OpenSource'
 	]);
 
-	$(".button-collapse").sideNav();
+	$('.button-collapse').sideNav();
 
-	$('a[href^="#"]').on('click', function (e) {
+	$('a[href^="#"]').on('click', function(e) {
 		e.preventDefault();
 
 		var target = this.hash;
 		var $target = $(target);
 
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top - 50
-		}, 600, 'swing', function () {
-			window.location.hash = target;
-		});
+		$('html, body')
+			.stop()
+			.animate(
+				{
+					scrollTop: $target.offset().top - 50
+				},
+				600,
+				'swing',
+				function() {
+					window.location.hash = target;
+				}
+			);
 	});
 
-	$("#message-form").on("submit", function () {
+	$('#message-form').on('submit', function() {
 		if ($('#message-form')[0].checkValidity()) {
 			$.ajax({
-				type: "post",
-				contentType: "application/x-www-form-urlencoded",
-				url: "/message",
+				type: 'post',
+				contentType: 'application/x-www-form-urlencoded',
+				url: '/message',
 				data: $('#message-form').serialize()
-			}).then(function (data) {
+			}).then(function(data) {
+				$('#send-btn')
+					.addClass('disabled')
+					.attr('disabled', true);
 
-				$("#send-btn")
-					.addClass("disabled")
-					.attr("disabled", true);
-
-				setTimeout(function () {
-					$("#send-btn")
-						.removeClass("disabled")
-						.removeAttr("disabled");
+				setTimeout(function() {
+					$('#send-btn')
+						.removeClass('disabled')
+						.removeAttr('disabled');
 				}, 60000);
 
-				Materialize.toast("Message sent", 3000, "green");
+				Materialize.toast('Message sent', 3000, 'green');
 
 				$('#message-form')[0].reset();
 			});
 		} else {
-			Materialize.toast("Please fill the form data properly.", 3000, "red");
+			Materialize.toast('Please fill the form data properly.', 3000, 'red');
 		}
 		return false;
 	});
 
+	$(document).ready(function() {
+		$('.carousel').carousel();
+	});
 })(jQuery, Materialize);
