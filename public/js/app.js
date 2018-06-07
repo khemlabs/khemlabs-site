d3c7e4bb9b49da11eadd5813f2421f52dcc60421
Pablo Gambetta <gambeita@gmail.com>
2018-06-07 17:44:53 -0300

    carrusel auto added

--- public/js/app.js
+++ public/js/app.js
@@ -67,5 +67,10 @@
 
 	$(document).ready(function() {
 		$('.carousel').carousel();
+		autoplay();
+		function autoplay() {
+			$('.carousel').carousel('next');
+			setTimeout(autoplay, 2000);
+		}
 	});
 })(jQuery, Materialize);
